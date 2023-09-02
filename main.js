3a85c185a14d8e1a455d1f1dac4dbdfcda259aef
Guerino Henrique <guerinohenrique@gmail.com>
2023-09-02 17:10:46 -0300

    exercicio feito

--- main.js
+++ main.js
@@ -1,43 +1,5 @@
-const arrayDeAlunos = [
-    {
-        nome: 'Aluno 1',
-        nota: 8
-    },
-    {
-        nome: 'Aluno 2',
-        nota: 5
-    },
-    {
-        nome: 'Aluno 3',
-        nota: 6
-    },
-    {
-        nome: 'Aluno 4',
-        nota: 4
-    },
-    {
-        nome: 'Aluno 5',
-        nota: 7
-    },
-    {
-        nome: 'Aluno 6',
-        nota: 10
-    },
-]
-
-const notaMinima = 6
-let alunosAprovados = []
-function consultaAProvados(){
-    
-    for(aluno of arrayDeAlunos){
-        let notaDeste = aluno.nota
-
-        if(notaDeste >= notaMinima){
-            alunosAprovados.push(aluno.nome)
-        }
-    }
-
-    console.log(alunosAprovados)
-}
-
-console.log(consultaAProvados())
+// "use strict";
+// const multiplicar = (valor1, valor2) => valor1 * valor2;
+// const dizOla = (nome) => 'Olá' + nome;
+// console.log(multiplicar(3, 2));
+// console.log(dizOla('Alguem'));
---
+++ main.ts
@@ -0,0 +1,6 @@
+let multiplicar = (valor1: number, valor2: number) => valor1 * valor2;
+let dizOla = (nome: string) => 'Olá' + nome
+
+console.log(multiplicar(3,2))
+console.log(dizOla('Alguem'))
+
