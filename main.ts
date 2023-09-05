43afddb0db002b222b68f8d2ae31a9b3f2a76cd6
Guerino Henrique <guerinohenrique@gmail.com>
2023-09-05 18:17:12 -0300

    tipagem na funcao

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-let multiplicar = (valor1: number, valor2: number) => valor1 * valor2;
-let dizOla = (nome: string) => 'Olá' + nome
+let multiplicar = (valor1: number, valor2: number): number => valor1 * valor2;
+let dizOla = (nome: string): string => 'Olá' + nome
 
 console.log(multiplicar(3,2))
 console.log(dizOla('Alguem'))
