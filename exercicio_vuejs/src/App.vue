02d37ca2d1821b04eaf65ba50f9d6e76f2290839
Guerino Henrique <guerinohenrique@gmail.com>
2023-09-13 17:48:29 -0300

    scss faz falta

--- exercicio_vuejs/src/App.vue
+++ exercicio_vuejs/src/App.vue
@@ -10,12 +10,12 @@
   })
 
   const recuperaCampo1 = (e) => {
-    estado.campo1 = parseInt(e.target.value) 
+    estado.campo1 = parseFloat(e.target.value) 
     operacao()
   }
 
   const recuperaCampo2 = (e) => {
-    estado.campo2 = parseInt(e.target.value)
+    estado.campo2 = parseFloat(e.target.value)
     operacao()
   }
 
@@ -25,23 +25,18 @@
   }
 
   const operacao = () => {
+    if(estado.operacao == 'soma'){
+    estado.resultado = estado.campo1 + estado.campo2
 
-    if(estado.campo1 != null && estado.campo2 != null){
-      if(estado.operacao == 'soma'){
-      estado.resultado = estado.campo1 + estado.campo2
-  
-      } else if(estado.operacao == 'subtracao'){
-        estado.resultado = estado.campo1 - estado.campo2
+    } else if(estado.operacao == 'subtracao'){
+      estado.resultado = estado.campo1 - estado.campo2
 
-      } else if(estado.operacao == 'multiplicacao'){
-        estado.resultado = estado.campo1 * estado.campo2
+    } else if(estado.operacao == 'multiplicacao'){
+      estado.resultado = estado.campo1 * estado.campo2
 
-      } else{
-        estado.resultado = estado.campo1 / estado.campo2
-      }
-    
+    } else{
+      estado.resultado = estado.campo1 / estado.campo2
     }
-
   }
 
     
@@ -51,11 +46,24 @@
   <div class="container">
     <Formulario :recupera-campo1="recuperaCampo1" :recupera-campo2="recuperaCampo2" :recupera-operador="recuperaOperador"></Formulario>
     <br>
-    <p>
-      Resultado: {{ estado.resultado }}
+    <p class="result">
+      Resultado: <span class="res">{{ estado.resultado }}</span>
     </p>
   </div>
 </template>
 
 <style scoped>
+.result{
+  text-align: center;
+  width: max-content;
+  font-size: large;
+  align-content: center;
+  margin-left: 64px;
+  border-bottom: 2px solid black;
+}
+
+.res{
+  font-weight: bold;
+  text-decoration: none;
+}
 </style>
--- exercicio_vuejs/src/components/Formulario.vue
+++ exercicio_vuejs/src/components/Formulario.vue
@@ -3,22 +3,50 @@
 </script>
 
 <template>
-    <form class="mt-5" action="">
+    <form class="formulario mt-5" action="">
         <div class="row">
-            <div class="col-md-1">
-                <input @keyup="recuperaCampo1" class="form-control" type="number" name="" id="">
+            <div class="col-md-2">
+                <input @keyup="recuperaCampo1" class="input form-control" type="number" name="" id="">
             </div>
-            <div class="col-md-1">
-                <select @change="recuperaOperador" name="" id="">
+            <div class="col-md-2">
+                <select class="form-control operador" @change="recuperaOperador" name="" id="">
                     <option value="soma">+</option>
                     <option value="subtracao">-</option>
                     <option value="multiplicacao">*</option>
                     <option value="divisao">/</option>
                 </select>
             </div>
-            <div class="col-md-1">
-                <input @keyup="recuperaCampo2" class="form-control" type="number" name="" id="">
+            <div class="col-md-2">
+                <input @keyup="recuperaCampo2" class="input form-control" type="number" name="" id="">
             </div>
         </div>
     </form>
 </template>
+
+<style scoped>
+.input{
+    border: 1px solid black;
+    -moz-appearance: textfield;
+    appearance: textfield;
+}
+
+.input::-webkit-inner-spin-button{
+    -webkit-appearance: none;
+}
+
+.input:hover{
+    cursor: pointer;
+}
+
+.operador{
+    border: 1px solid black;
+    height: 100%;
+    text-align: center;
+    display: block;
+    margin: 0 auto;
+}
+.operador:hover{
+    cursor: pointer;
+}
+
+</style>
